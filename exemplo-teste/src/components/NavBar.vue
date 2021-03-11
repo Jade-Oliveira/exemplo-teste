b4f7319803b0dff53b9d5ac85343a14ce32f4f82
Jade Oliveira <jade.ufrgs@gmail.com>
2021-03-11 15:02:11 -0300

    Atualizando layout

---
+++ exemplo-teste/src/components/NavBar.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <v-app-bar
+      color="blue"
+      dense
+      dark
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title class="title1">Projeto</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+     
+      <v-menu
+        left
+        bottom
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            icon
+            v-bind="attrs"
+            v-on="on"
+          >
+
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item
+            v-for="n in 5"
+            :key="n"
+            @click="() => {}"
+          >
+            <v-list-item-title>Option {{ n }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+    <v-img class="first-img" src="https://www.lopes.com.br/blog/wp-content/uploads/2017/02/casas-bonitas.jpg" alt="Gatinhos"></v-img>
+
+  </div>
+</template>
+
+<script>
+export default {
+   name: 'NavBar'
+}
+</script>
+
+<style scoped>
+  .first-img {
+    margin: 15px;
+  }
+</style>
---
+++ exemplo-teste/src/components/ProductsInfos.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id="products">
+        <div id="products1">
+            <h3 class="infos">Cabo Frio - RJ</h3><br>
+            <h3 class="infos">João Garcia</h3>
+        </div>
+        <div id="products2">
+            <h3 class="infos">R$ 5,000.000</h3><br>
+            <h3 class="infos">Venda</h3>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+   name: 'ProductsInfos'
+}
+</script>
+
+<style scoped>
+    #products {
+        margin: 15px;
+        display: flex;
+        justify-content: space-between;
+    }
+
+    #products2 {
+    text-align: end;
+
+    }
+
+    .infos {
+        color: rgb(43, 39, 39);
+        font-weight: 400;
+        font-size: 18px;
+        line-height: 5px;
+    }
+
+
+</style>
